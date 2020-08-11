66d426de1d8c1407636adbcec785cb9439c5b7fd
JACQUES Hugo <20521391+hugojcqs@users.noreply.github.com>
2020-08-11 23:52:11 +0200

    Updt otp.py to prevent useless call to byte_secret

--- src/pyotp/otp.py
+++ src/pyotp/otp.py
@@ -26,7 +26,7 @@ class OTP(object):
         """
         self.digits = digits
         self.digest = digest
-        self.secret = s
+        self.secret = OTP.byte_secret(s)
         self.name = name or 'Secret'
         self.issuer = issuer
 
@@ -37,7 +37,7 @@ class OTP(object):
         """
         if input < 0:
             raise ValueError('input must be positive integer')
-        hasher = hmac.new(self.byte_secret(), self.int_to_bytestring(input), self.digest)
+        hasher = hmac.new(self.secret, self.int_to_bytestring(input), self.digest)
         hmac_hash = bytearray(hasher.digest())
         offset = hmac_hash[-1] & 0xf
         code = ((hmac_hash[offset] & 0x7f) << 24 |
@@ -49,12 +49,13 @@ class OTP(object):
             str_code = '0' + str_code
 
         return str_code
-
-    def byte_secret(self) -> bytes:
-        missing_padding = len(self.secret) % 8
+    
+    @staticmethod
+    def byte_secret(secret) -> bytes:
+        missing_padding = len(secret) % 8
         if missing_padding != 0:
-            self.secret += '=' * (8 - missing_padding)
-        return base64.b32decode(self.secret, casefold=True)
+            secret += '=' * (8 - missing_padding)
+        return base64.b32decode(secret, casefold=True)
 
     @staticmethod
     def int_to_bytestring(i: int, padding: int = 8) -> bytes:
